bf2301a15510e4117038c68369f427a35a9d2fd6
sophieliu <2461510738@qq.com>
2025-04-05 18:37:04 +0800

    登录和注册成功实现

---
+++ src/test/java/com/vms/cnlearning/CnLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vms.cnlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
